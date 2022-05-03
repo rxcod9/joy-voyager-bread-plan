2c6caf4175fe157b4f0331afed34ab66f44802a5
Ramakant <gangwar.ramakant@gmail.com>
2022-05-03 19:24:06 +0530

    💚_Seeder-count

--- database/seeders/DummyPlansTableSeeder.php
+++ database/seeders/DummyPlansTableSeeder.php
@@ -14,7 +14,7 @@ class DummyPlansTableSeeder extends Seeder
      */
     public function run()
     {
-        $count = 100;
+        $count = 20;
         Plan::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
--- database/seeders/PlansTableSeeder.php
+++ database/seeders/PlansTableSeeder.php
@@ -18,7 +18,7 @@ class PlansTableSeeder extends Seeder
             return false;
         }
 
-        $count = 100;
+        $count = 20;
         Plan::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
